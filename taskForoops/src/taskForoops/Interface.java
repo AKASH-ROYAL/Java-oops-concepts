826497d5dcd903115b60fef0060e77b61d77f7f4
AKASH-ROYAL <115221061+AKASH-ROYAL@users.noreply.github.com>
2022-11-02 15:16:02 -0700

    Interface updated

--- taskForoops/src/taskForoops/Interface.java
+++ taskForoops/src/taskForoops/Interface.java
@@ -1,5 +1,29 @@
 package taskForoops;
 
-public class Interface {
+interface Interface1 {
+	  public void  Method1();                            // First interface method
+	}
 
-}
+	interface  Interface2 {
+	  public void  Method2();                       // Second interface method
+	}
+
+	
+	class DemoClass implements  Interface1,  Interface2 {                  // Multiple interfaces or inheritance
+	  public void Method1() {
+	    System.out.println("Some text..");
+	  }
+	  public void Method2() {
+	    System.out.println("Some other text...");
+	  }
+	}
+
+	public class Interface{
+	  public static void main(String[] args) {
+	    DemoClass myObj = new DemoClass();
+	    myObj. Method1();
+	    myObj.  Method2();
+	  }
+	}
+
+ 
