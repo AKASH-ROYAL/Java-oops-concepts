730929373c8caed340150ef8b7dbfafab87d32f4
AKASH-ROYAL <115221061+AKASH-ROYAL@users.noreply.github.com>
2022-11-07 09:46:22 -0800

    Getter setter

--- taskForoops/src/taskForoops/Encapsulation.java
+++ taskForoops/src/taskForoops/Encapsulation.java
@@ -2,25 +2,27 @@ package taskForoops;
 
  class Encapsulation {
 	 int a;
-	 public int getA() {
+	 public int getA() {             // getter method
 		return a;
 	}
-	public void setA(int a) {
+	public void setA(int a) {        //setter method
 		this.a = a;
 	}
-	public int getB() {
-		return b;
-	}
-	public void setB(int b) {
-		this.b = b;
-	}
+	
  
-	int b;
+
  public static void main (String[] args) {
 	 Encapsulation  a =new Encapsulation();
 	
-    a.setA(5);
+    a.setA(5555);
     int x= a.getA();
 	 System.out.println(x);
  }
 }
+// public int getB() {
+//		return b;
+//	}
+//	public void setB(int b) {
+//		this.b = b;
+//	}
+//	int b;
